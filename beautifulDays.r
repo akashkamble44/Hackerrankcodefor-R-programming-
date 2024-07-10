c4b258fd1e8b00bee5b1c54272aaed919eb52fb8
AKASH CHANDRAKANT  KAMBLE <86791132+akashkamble44@users.noreply.github.com>
2024-07-10 12:02:40 +0530

    beautifulDays.r

--- beautifulDays.r
+++ beautifulDays.r
@@ -17,3 +17,20 @@ beautifulDays <- function(i, j, k) {
   }
   return(count)
 }
+superDigit <- function(n, k) {
+  # Helper function to compute the sum of digits of a number
+  sumDigits <- function(x) {
+    sum(as.numeric(unlist(strsplit(as.character(x), split = ""))))
+  }
+  
+  # Compute the initial sum of digits of n
+  initialSum <- sumDigits(n)
+  
+  # Calculate the super digit
+  superDigit <- (initialSum * k) %% 9
+  if (superDigit == 0 && initialSum * k != 0) {
+    superDigit <- 9
+  }
+  
+  return(superDigit)
+}
